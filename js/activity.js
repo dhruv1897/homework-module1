47c06c641b654458761a4d6d9300b93ddeb827d1
dhruv1897 <66731309+dhruv1897@users.noreply.github.com>
2024-02-06 13:48:04 -0800

    Remove all try code comment.

--- js/activity.js
+++ js/activity.js
@@ -1,9 +1,5 @@
 $(document).ready(function() { 
     // Highlight the selected activity cell
-//     $(document).bind("click", function (e) { 
-//         $(e.target).closest("td").toggleClass("Selected");   
-// })
-
     $('table tbody td').not(':first-child').on('click', function() {
         if ($(this).text() !== 'Not Available') {
             $(this).toggleClass("selected");
@@ -21,5 +17,3 @@ $(document).ready(function() {
 
 });
 
-// $(document).bind("click", function (e) { 
-    // $(e.target).closest("li").toggleClass("highlight");
